c06d80c20a1344a99e693b4bea39c49439573183
Truc350 <23130350@st.hcmuaf.edu.vn>
2024-12-24 16:18:06 +0700

    Set layout lai

--- src/view/PanelHeaderOfHome.java
+++ src/view/PanelHeaderOfHome.java
@@ -2,23 +2,30 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanelHeaderOfHome extends JPanel{
-    JLabel labelLogo ;
+    JButton btnLogo ;
     JLabel titleLable;
 
-    public PanelHeaderOfHome() {
+    public PanelHeaderOfHome(JPanel panelCard, CardLayout cardLayout) {
         this.setBackground(new Color(59,185,94));
         this.setBounds(10,10,80,80);
         this.setLayout(new BorderLayout());
 
         //  add logo//
-//        btnLogo = new JButton(new ImageIcon("src/img/Logo_HCMUAF.svg.png"));
-//        btnLogo.setBackground(new Color(59,185,94));
-        ImageIcon logo = new ImageIcon("src/img/Logo_HCMUAF.svg.png");
-        labelLogo =  new JLabel(logo);
-        labelLogo.setBounds(10,10,80,80);
-        this.add(labelLogo,BorderLayout.WEST);
+        btnLogo = new JButton(new ImageIcon("src/img/Logo_HCMUAF.svg.png"));
+        btnLogo.setBackground(new Color(59,185,94));
+        btnLogo.setBounds(10,10,80,80);
+        btnLogo.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(panelCard,"chooseStudentOrManager");
+            }
+        });
+        this.add(btnLogo,BorderLayout.WEST);
 
         //add title
         titleLable = new JLabel("Hệ Thống Quản Lý KTX NLU");
